8f8cc61dd02d102b8f3db2cd5c2be4aaf0ce4bf1
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2022-10-26 20:43:31 +0500

    changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,31 +17,52 @@ function Footer() {
   let loginUser = JSON.parse(localStorage.getItem("user") || "{}");
 
   return (
-    <AppBar
-      position="static"
-      sx={{ bgcolor: "", color: "text.primary" }}
-      className="px-7 py-7 sm:px-7 md:px-7 md:py-7"
-    >
-      <Toolbar className="flex gap-x-1">
-        <Grid container spacing={5}>
-          <Grid
-            xs={12}
-            sm={12}
-            md={4}
-            className="flex flex-col gap-y-2 justify-center"
-          >
-            <Typography color="white" variant="h6">
-              PERSONAL COLLECTION
-            </Typography>
-            <Typography color="white" variant="body2">
-              Personal Collection is a direct selling company that provides high
-              quality products and livelihood through its dealership network.
-            </Typography>
-          </Grid>
-          <Grid xs={12} sm={12} md={4} className="flex flex-col gap-y-2">
-            {pages.map((page, index) => {
-              if (page.title === "personal") {
-                if (loginUser.role) {
+    <Box className="mt-auto">
+      <AppBar
+        position="static"
+        sx={{ bgcolor: "", color: "text.primary" }}
+        className="px-6 py-7 sm:px-3 md:px-7 md:py-7"
+      >
+        <Toolbar className="flex gap-x-1">
+          <Grid container spacing={5}>
+            <Grid
+              xs={12}
+              sm={12}
+              md={4}
+              className="flex flex-col gap-y-2 justify-center"
+            >
+              <Typography color="white" variant="h6">
+                PERSONAL COLLECTION
+              </Typography>
+              <Typography color="white" variant="body2">
+                Personal Collection is a direct selling company that provides
+                high quality products and livelihood through its dealership
+                network.
+              </Typography>
+            </Grid>
+            <Grid xs={12} sm={12} md={4} className="flex flex-col gap-y-2">
+              {pages.map((page, index) => {
+                if (page.title === "personal") {
+                  if (loginUser.role) {
+                    return (
+                      <NavLink
+                        to={page.link}
+                        key={index}
+                        className="no-underline"
+                      >
+                        <Typography
+                          color="white"
+                          className="capitalize"
+                          variant="body2"
+                        >
+                          {t(`${page.title}`)}
+                        </Typography>
+                      </NavLink>
+                    );
+                  } else {
+                    return null;
+                  }
+                } else {
                   return (
                     <NavLink
                       to={page.link}
@@ -57,37 +78,29 @@ function Footer() {
                       </Typography>
                     </NavLink>
                   );
-                } else {
-                  return null;
                 }
-              } else {
-                return (
-                  <NavLink to={page.link} key={index} className="no-underline">
-                    <Typography
-                      color="white"
-                      className="capitalize"
-                      variant="body2"
-                    >
-                      {t(`${page.title}`)}
-                    </Typography>
-                  </NavLink>
-                );
-              }
-            })}
-          </Grid>
+              })}
+            </Grid>
 
-          <Grid xs={12} sm={12} md={4}>
-            <Typography color="white" variant="body2">
-              Ground Floor, Triumph Building 1610 Quezon Avenue, Diliman, Quezon
-              City, 1104 Metro Manila
-            </Typography>
-            <Typography color="white" variant="body1">
-              (02) 8 376 6888
-            </Typography>
+            <Grid xs={12} sm={12} md={4}>
+              <Typography color="white" variant="body2">
+                Ground Floor, Triumph Building 1610 Quezon Avenue, Diliman,
+                Quezon City, 1104 Metro Manila
+              </Typography>
+              <Typography color="white" variant="body1">
+                (02) 8 376 6888
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <Box
+        className="px-13 sm:px-7 md:px-14"
+        sx={{ bgcolor: "#F3F2EF", fontSize: "10px" }}
+      >
+        Personal Collection Corporation © 2022
+      </Box>
+    </Box>
   );
 }
 
--- src/pages/Personal/Personal.tsx
+++ src/pages/Personal/Personal.tsx
@@ -101,7 +101,7 @@ function Personal() {
           Create Collection
         </Button>
       </Box>
-      <Grid container spacing={{ xs: 2, md: 3 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} className="min-h-[400px]">
         {!collectionListLoading
           ? collectionList.map((item, index) => (
               <Grid xs={12} sm={6} md={4} lg={3} key={item.id}>
